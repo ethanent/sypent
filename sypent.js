2997b330547b5d249db76281c16ed2dbcebd6c03
Saul van der Walt <saulvdw@gmail.com>
2018-02-25 11:06:18 +0200

    use .append(...text/node) and reduce complexity

--- sypent.js
+++ sypent.js
@@ -1,31 +1,17 @@
-{ /* global Text Node */
-  const isStr = o => typeof o === 'string'
-  const isNode = o => o instanceof Node
+var sy = new Proxy((tag, attribs, ...children) => {
+  if (!tag) throw new Error('Missing tag argument.')
+  const el = document.createElement(tag)
 
-  window.sy = new Proxy((tag, attribs, ...children) => {
-    if (!tag) throw new Error('Missing tag argument.')
-
-    const el = document.createElement(tag)
-
-    if (isNode(attribs) || isStr(attribs)) children.unshift(attribs)
-    else if (attribs) {
-      for (const key in attribs) {
-        const val = attribs[key]
-        if (key in el) el[key] = val
-        else if (isStr(val)) {
-          el.setAttribute(key, val)
-        } else {
-          Object.defineProperty(el, key, Object.getOwnPropertyDescriptor(attribs, key))
-        }
-      }
+  if (attribs instanceof Node || typeof attribs === 'string') {
+    children.unshift(attribs)
+  } else if (attribs === Object(attribs)) {
+    for (const key in attribs) {
+      key in el ? el[key] = attribs[key] : el.setAttribute(key, attribs[key])
     }
+  }
 
-    children.length && children.forEach(child => {
-      el.appendChild(isNode(child) ? child : new Text(child))
-    })
-
-    return el
-  }, {
-    get: (fn, key) => fn.bind(null, key)
-  })
-}
+  el.append(...children)
+  return el
+}, {
+  get: (fn, key) => fn.bind(null, key)
+})
